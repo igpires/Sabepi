7b564be8aa8dbfc37e2df3562b64317b128d63b1
Igor Pires <pires.igor2@gmail.com>
2022-05-04 20:14:23 -0300

    add column is_active in to the classroom

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_03_183117) do
+ActiveRecord::Schema.define(version: 2022_05_04_193126) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -44,6 +44,7 @@ ActiveRecord::Schema.define(version: 2022_05_03_183117) do
     t.bigint "subject_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean "is_active", default: false
     t.index ["subject_id"], name: "index_classrooms_on_subject_id"
     t.index ["user_id"], name: "index_classrooms_on_user_id"
   end
