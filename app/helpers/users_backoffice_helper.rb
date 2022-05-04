aa6a25526d98e6d23e262cf0b1c9eb05f05f3af0
Igor Pires <pires.igor2@gmail.com>
2022-05-04 20:18:27 -0300

    create a helper to verify if classroom is active

--- app/helpers/users_backoffice_helper.rb
+++ app/helpers/users_backoffice_helper.rb
@@ -1,2 +1,12 @@
 module UsersBackofficeHelper
+    def classroom_status(is_active)
+        if is_active
+            status = '<color class="text-success"> Ativada </color>'
+            status.html_safe
+        else
+            status = '<color class="text-danger"> Desativada </color>'
+            status.html_safe
+        end
+
+    end
 end
