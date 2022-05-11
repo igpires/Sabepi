5cc69aeac9365df026fab5ed8f53a74d573b9559
Igor Pires <pires.igor2@gmail.com>
2022-05-11 14:58:30 -0300

    increases range of possible access codes

--- app/models/classroom.rb
+++ app/models/classroom.rb
@@ -5,7 +5,7 @@ class Classroom < ApplicationRecord
   # ========= References =========
   belongs_to :user
   belongs_to :subject
-
+  has_many :class_occurrences
   # ======== Private Method =======
   private
 
@@ -15,7 +15,7 @@ class Classroom < ApplicationRecord
 
   def generate_code
     loop do
-      code = SecureRandom.base64(3)
+      code = SecureRandom.base64(3.5)
       break code unless Classroom.where(access_code: code).exists?
     end
   end
