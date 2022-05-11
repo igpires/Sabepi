277e67ec7d03f52b09f7252302975ad7c7427b73
Igor Pires <pires.igor2@gmail.com>
2022-05-11 14:42:14 -0300

    create a helper for return a toggle switch for classroom status

--- app/helpers/users_backoffice_helper.rb
+++ app/helpers/users_backoffice_helper.rb
@@ -1,12 +1,35 @@
 module UsersBackofficeHelper
-    def classroom_status(is_active)
-        if is_active
-            status = '<color class="text-success font-weight-bold"> Ativada </color>'
+    
+    def classroom_statuss(classroom)
+        if classroom.is_active
+            status = '<color id=status-color' + classroom.id.to_s + ' class="text-success font-weight-bold"> Ativada </color>'
+            
             status.html_safe
         else
-            status = '<color class="text-danger font-weight-bold"> Desativada </color>'
+            status = '<color id=' + classroom.id.to_s + ' class="text-danger font-weight-bold"> Desativada </color>'
+            
             status.html_safe
         end
-
     end
+    def classroom_status(classroom)
+        if classroom.is_active
+            status ='<div class="custom-control custom-switch">
+                        <input type="checkbox" checked class="custom-control-input" id=' + classroom.id.to_s + '>
+                        <label class="custom-control-label text-success font-weight-bold" for=' + classroom.id.to_s + '> Ativada </label>
+                    </div>'
+            
+            status.html_safe
+        else
+            status ='<div class="custom-control custom-switch">
+                        <input type="checkbox" class="custom-control-input" id=' + classroom.id.to_s + '>
+                        <label class="custom-control-label text-danger font-weight-bold" for=' + classroom.id.to_s + '> Desativada </label>
+                    </div>'
+            status.html_safe
+        end
+    end
+
+
+    
 end
+
+                        
