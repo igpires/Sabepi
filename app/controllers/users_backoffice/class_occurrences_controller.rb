4436c9e8eed1e672dc52dcc84a2e26c5d8d8ef36
Igor Pires <pires.igor2@gmail.com>
2022-05-12 15:28:41 -0300

    add topics to index

--- app/controllers/users_backoffice/class_occurrences_controller.rb
+++ app/controllers/users_backoffice/class_occurrences_controller.rb
@@ -2,7 +2,9 @@ class UsersBackoffice::ClassOccurrencesController < UsersBackofficeController
   before_action :set_class_occurrence, only: [:edit, :update, :destroy]
 
   def index
+    subject_id = current_user.classrooms.find_by(id: params[:id]).subject_id
     @class_occurrences = current_user.classrooms.find_by(id: params[:id]).class_occurrences.order(created_at: :desc)
+    @topics = Topic.where(subject_id: subject_id)
   end
 
   def new
@@ -12,7 +14,7 @@ class UsersBackoffice::ClassOccurrencesController < UsersBackofficeController
   def create
     @class_occurrence = ClassOccurrence.new(params_class_occurrence)
     if @class_occurrence.save()
-      redirect_to admins_backoffice_class_occurrences_path, notice: "curso cadastrado com sucesso!"
+      redirect_to admins_backoffice_class_occurrences_path, notice: "Aula criada com sucesso!"
     else
       render :edit
     end
