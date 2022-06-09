eb44f12489e28b7b76a37123e104e09ffade5060
Igor Pires <pires.igor2@gmail.com>
2022-06-09 12:04:53 -0300

    fix params name

--- app/controllers/users_backoffice/class_occurrences_controller.rb
+++ app/controllers/users_backoffice/class_occurrences_controller.rb
@@ -2,8 +2,8 @@ class UsersBackoffice::ClassOccurrencesController < UsersBackofficeController
   before_action :set_class_occurrence, only: [:change_question, :show_occurrence]
 
   def index
-    subject_id = current_user.classrooms.find_by(id: params[:id]).subject_id
-    @class_occurrences = current_user.classrooms.find_by(id: params[:id]).class_occurrences.order(created_at: :desc)
+    subject_id = current_user.classrooms.find_by(id: params[:classroom_id]).subject_id
+    @class_occurrences = current_user.classrooms.find_by(id: params[:classroom_id]).class_occurrences.order(created_at: :desc)
     @topics = Topic.where(subject_id: subject_id)
   end
 
@@ -11,11 +11,11 @@ class UsersBackoffice::ClassOccurrencesController < UsersBackofficeController
     @class_occurrence = ClassOccurrence.new(params_class_occurrence)
     class_id = @class_occurrence.classroom_id
     if occurrence_on_day?(class_id)
-      redirect_to users_backoffice_class_occurrences_path(id: class_id), alert: "Voce nao pode criar mais de uma aula no dia!"
+      redirect_to users_backoffice_class_occurrences_path(classroom_id: class_id), alert: "Voce nao pode criar mais de uma aula no dia!"
     elsif @class_occurrence.save()
-      redirect_to users_backoffice_class_occurrences_path(id: class_id), notice: "Aula criada com sucesso!"
+      redirect_to users_backoffice_class_occurrences_path(classroom_id: class_id), notice: "Aula criada com sucesso!"
     else
-      redirect_to users_backoffice_class_occurrences_path(id: class_id), alert: "Erro ao criar sala!"
+      redirect_to users_backoffice_class_occurrences_path(classroom_id: class_id), alert: "Erro ao criar sala!"
     end
   end
 
